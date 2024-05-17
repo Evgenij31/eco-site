312755cb12ecff51b4b552cea3979c071c223a1f
Evgenij31 <evgenijjosifovski@gmail.com>
2024-05-17 19:53:31 +0200

    added responsive and animations

---
+++ app.js
@@ -0,0 +1,27 @@
+//mobile navigation script
+let closeNavBtn = document.getElementById("close-nav-btn");
+let openNavBtn = document.getElementById("open-nav-btn");
+let navUl = document.getElementById("nav-ul");
+let logo = document.getElementById("logo");
+
+//on click open the navigation
+function openNav() {
+    openNavBtn.style.display = "none";
+    closeNavBtn.style.animation = "scale-in .2s";
+    setTimeout(() => {
+        closeNavBtn.style.display = "block";
+    }, 200);
+    logo.style.display = "none";
+    navUl.style.display = "block";
+}
+
+function closeNav() {
+    closeNavBtn.style.display = "none";
+    openNavBtn.style.animation = "scale-in .2s";
+    setTimeout(() => {
+        openNavBtn.style.display = "block";
+    }, 200);
+
+    logo.style.display = "block";
+    navUl.style.display = "none";
+}
